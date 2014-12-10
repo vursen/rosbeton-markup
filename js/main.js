4760700dd0a04176f0d97f67bf81d266de3ae688
Sergey Vinogradov <pettsonchello@gmail.com>
2014-12-10 22:22:37 +0300

    Релиз

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
-$(document).ready(function() {
+$(window).load(function() {
 
-    
+  var $portfolio = $('.b-portfolio');
+
+  $portfolio.find('.b-portfolio__slider').sly({
+    horizontal: 1,
+    itemNav: 'centered',
+    smart: 1,
+    prevPage: $portfolio.find('.b-portfolio__controls__prev'),
+    nextPage: $portfolio.find('.b-portfolio__controls__next')
+  })   
 
 });
